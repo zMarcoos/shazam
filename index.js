a5754375352e30041a43026d8757750bcafd23c7
zMarcoos <59182008+zMarcoos@users.noreply.github.com>
2020-08-08 11:20:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  const ping = new Date();
+  ping.setHours(ping.getHours() - 3);
+  console.log(`Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`);
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
 const Discord = require('discord.js');
 const client = new Discord.Client({disableEveryone: false});
 require("./functions")(client);
